ee3af7fe6f40a3e45b8fcca8a7e28e915ffe70d7
peytonhowell <peytonhowelle@gmail.com>
2021-02-01 00:51:30 -0500

    create screen functional

--- app/src/main/java/com/example/twistontime/CreateTimerScreen.java
+++ app/src/main/java/com/example/twistontime/CreateTimerScreen.java
@@ -1,14 +1,66 @@
 package com.example.twistontime;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateTimerScreen extends AppCompatActivity {
 
+    Timer timer = new Timer();
+    class Timer {
+        public String name;
+        public Time duration;
+        public String repeat;
+        public String notification;
+        public String sound;
+        public boolean preset = false;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_timer_screen);
+
+        CheckBox preset = (CheckBox) findViewById(R.id.checkBox);
+        preset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.preset = !timer.preset;
+                System.out.println(timer.preset);
+            }});
+        EditText name = (EditText) findViewById(R.id.timerName);
+        Button repeat_button = (Button)findViewById(R.id.changeRepeatButton);
+        repeat_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateTimerScreen.this, CustomizeRepeatScreen.class));
+            }
+        });
+        Button notification_button = (Button)findViewById(R.id.addNotificationButton);
+        notification_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateTimerScreen.this, CustomizeNotificationScreen.class));
+            }
+        });
+        Button sound_button = (Button)findViewById(R.id.changeSoundButton);
+        sound_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateTimerScreen.this, CustomizeSoundsScreen.class));
+            }
+        });
+        Button submit_button = (Button)findViewById(R.id.submit);
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateTimerScreen.this, MainActivity.class));
+            }
+        });
     }
 }
